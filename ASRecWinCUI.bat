324a4984c33fb780480e36bff9e2e7ed2ef43de6
wakanameko <63937252+wakanameko@users.noreply.github.com>
2022-07-26 10:49:30 +0900

    Create ASRecWinCUI.bat

---
+++ ASRecWinCUI.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ================================
+echo  = AndroidScreenRecorder v1.0 =
+echo  =      by @wakanameko2       =
+echo ================================
+pause
+
+::select
